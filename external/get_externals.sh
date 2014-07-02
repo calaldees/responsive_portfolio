a481de8fa4fca796a3453488695a7e06438a93a8
Allan Callaghan <allan.callaghan@thisisglobal.com>
2014-07-02 19:42:38 +0100

    Inital portfolio concept Uses Masonry

---
+++ external/get_externals.sh
@@ -0,0 +1,4 @@
+curl http://thing/jquery.min.js -O
+curl http://thing/masonry.min.js -O
+curl http://thing/css_reset.css
+
---
+++ portfolio_items.js
@@ -0,0 +1,626 @@
+build_masonry(
+[
+    {
+        'title':'Global Radio',
+        'link': 'www.thisisglobal.com',
+        'image': '',
+        'description': "",
+        'year': '2012-present',
+        'team': 'group',
+        'language': ['py','js'],
+        'category': 'code',
+        'width': 3,
+        'height': 3
+    },
+
+    {
+        'title':'KaraKara',
+        'link': 'https://github.com/calaldees/KaraKara',
+        'image': 'karakara.png',
+        'description': "Karaoke event system. Users can search/preview/queue tracks from their mobile phones. Project Lead.",
+        'year': '2012-2014',
+        'team': 'group',
+        'language': ['py','js'],
+        'category': 'code',
+        'width': 2,
+        'height': 3
+    },
+    
+    {
+        'title': 'Pentatonic Hero',
+        'width': 1,
+        'height': 1
+    },
+
+    {
+        'title': 'VoteBattle',
+        'width': 1,
+        'height': 2
+    },
+
+    {
+        'title': 'TrackMy',
+        'width': 1,
+        'height': 1
+    },
+
+    
+    {
+        'title':'Flexi RPG Responsve Digital Pubication',
+        'link': '',
+        'image': '',
+        'description': "",
+        'year':'2013-2014',
+        'team': 'group',
+        'language': ['py','js'],
+        'category': 'code',
+        'width': 2,
+        'height': 1
+    },
+    
+    {
+        'title': 'Ayacon: Live Music Pannel',
+        'category': 'code',
+        'width': 2,
+        'height': 2
+    },
+
+    
+    {
+        'title': 'taxpayers alliance',
+        'width': 1,
+        'height': 1
+    },
+    
+    {
+        'title': 'RetroPrice Statistics',
+        'category': 'code',
+        'width': 2,
+        'height': 1
+    },
+    
+    {
+        'title': 'Streets of DIFID',
+        'width': 1,
+        'height': 2,
+    },
+    
+    {
+        'title':'Teach Programming',
+        'link': 'http://calaldees.tnkd.net/project/game/copter.html',
+        'image': 'teach_programming.png',
+        'description': "In progress. Learn to program multiplayer games in HTML5/Javascript, Python, Java, VB.NET and PHP. Teaching resources and activities.",
+        'year':'2012',
+        'team': 'solo',
+        'language': ['py','java','vb','php','js'],
+        'category': 'code',
+        'width': 3,
+        'height': 1
+    },
+    {
+        'title': 'Open mic performances',
+        'category': 'code',
+        'width': 1,
+        'height': 1
+    },
+
+    {
+        'title': 'Access Europe',
+        'width': 1,
+        'height': 1
+    },
+    
+    {
+        'title':'Civicboom',
+        'link': 'http://www.civicboom.com',
+        'image': 'civicboom.png',
+        'description': "Lead developer of a team of four. A citizen journalism platform with an API, Mobile apps, Media uploads, Geopositioning, QRCode generation, Facebook/Twitter integration, Segmented server architecture. 60,000 lines of Python/HTML/JS code with formal bug tracking using the agile methadology.",
+        'year':'2009-2012',
+        'team': 'group',
+        'language': ['py','js'],
+        'category': 'code',
+        'width': 3,
+        'height': 3
+    },
+    
+    {
+        'title': 'Coastal Cycle',
+        'year': '2010',
+        'description': 'Canterbury-Hastings-Brightion-Arendel-Southempton',
+        'category': 'code',
+        'width': 1,
+        'height': 1
+    },
+
+    
+    {
+        'title':'MultiPong',
+        'link': 'https://github.com/calaldees/MultiPong',
+        'image': 'multipong.png',
+        'description': "A three projector pong game with mobile phones as input devices.",
+        'year':'2012',
+        'team': 'group',
+        'language': ['py'],
+        'category': 'code',
+        'width': 1,
+        'height': 1
+    },
+    {
+        'title':'CarMuse',
+        'link': 'http://hacks.rewiredstate.org/events/power-of-minds/car-muse',
+        'image': 'carmuse.jpg',
+        'description': "Creates pseudo random music based on the output of the engine management system of a car. Car simulated in 3D.",
+        'description_extended': "I wrote the music generator based on speed, gear, indicators, breaks, etc.",
+        'year':'2011',
+        'team': 'group',
+        'language': ['py'],
+        'category': 'code',
+        'width': 1,
+        'height': 2
+    },
+    {
+        'title':'Parliamentary',
+        'link': 'http://rewiredstate.org/projects/parliamentary',
+        'image': '',
+        'description': "Hack weekend project. Every word said in parliament is logged. We process this and create a word cloud browser to browse what politicians are talking about.",
+        'year':'2011',
+        'team': 'group',
+        'language': ['py'],
+        'category': 'code',
+        'width': 2,
+        'height': 1
+    },
+    {
+        'title':'Barcode Thing',
+        'link': 'http://rewiredstate.org/projects/barcode-thing',
+        'image': 'barcode.jpeg',
+        'description': "Hack weekend project. Online barcode catalog and info retrieval. I wrote the Javascript algorithm to decode a barcode image from the phone camera.",
+        'description_extended': "Scan a barcode from your phone, server gives you, average price on eBay, if it's in your collection, previews of media e.g trailer from youtube, amazon mp3 previews of songs.",
+        'year':'2011',
+        'team': 'group',
+        'language': ['js'],
+        'category': 'code',
+        'width': 2,
+        'height': 1
+    },
+    {
+        'title':'FreeHoc',
+        'link': 'http://rewiredstate.org/projects/freehoc',
+        'image': 'freehoc.jpg',
+        'description': "Hack weekend project. Freecycle + Geopositional + Custom alerts + SMS interface.",
+        'year':'2011',
+        'team': 'group',
+        'language': ['py'],
+        'category': 'code',
+        'width': 2,
+        'height': 1
+    },
+    
+    {
+        'title': 'luggage',
+        'category': 'code',
+        'width': 2,
+        'height': 1
+    },
+    
+    {
+        'title': 'Nessessary Evil',
+        'description': '',
+        'year': '2009-2011',
+        'category': 'code',
+        'width': 3,
+        'height': 1
+    },
+    
+    {
+        'title': 'Piano Transcriptions',
+        'category': 'code',
+        'width': 1,
+        'height': 2
+    },
+
+    {
+        'title': 'AQA Computing',
+        'year': '2009-2011',
+        'description': '1 day a week',
+        'width': 1,
+        'height': 2,
+    },
+    
+    {
+        'title':'Guardian Heroes Sprites',
+        'link': '',
+        'image': 'ghrev1.png',
+        'description': "Attempt at reverse engineering the sprite format for sprites in the Sega Saturn game.",
+        'description_extended': "Injecting binary data into the ISO and using an emulator with a specific state to read the sprites from disk. Once the corruption had been observed I could makes notes as to algorithum that was processing the data.",
+        'year':'2011',
+        'team': 'solo',
+        'language': ['java'],
+        'category': 'code',
+        'width': 1,
+        'height': 1
+    },
+    
+    {
+        'title': 'k.alt.del',
+        'year': '2009-2011',
+        'description': '',
+        'category': 'code',
+        'width': 2,
+        'height': 1
+    },
+
+    
+    {
+        'title':'QuizGraphs',
+        'link': 'http://calaldees.dreamhosters.com/AnimeSoc/AnimeQuiz2011/graph.py',
+        'image': 'quizgraphs.jpg',
+        'description': "Pub quiz graph system with separate input interface.",
+        'year':'2010',
+        'team': 'solo',
+        'language': ['py'],
+        'category': 'code',
+        'width': 1,
+        'height': 1
+    },
+
+    {
+        'title': 'Sandwich Technology School',
+        'link': '',
+        'image': '',
+        'description': "AQA AS/A2 Computing. Solo taught a cohort thought. From 13 to 18",
+        'team': 'solo',
+        'year': '2007-2009',
+        'category': 'code',
+        'width': 2,
+        'height': 2
+    },
+
+    
+    {
+        'title':'3D Camera Rig and Processing',
+        'link': '',
+        'image': '3d.png',
+        'description': "Bolting two cameras side by side. Python code cyan/red merged the frames.",
+        'year': '2009',
+        'team': 'solo',
+        'language': ['py'],
+        'category': 'code',
+        'width': 2,
+        'height': 1
+    },
+
+
+    
+    
+    {
+        'title':'PostOffice Server',
+        'link': 'https://github.com/calaldees/javalib/tree/master/lib/Utils/Utils/Network/PostOfficeServer',
+        'image': '',
+        'description': "A key value server similar to Redis for teaching simple massively multiplayer games.",
+        'description_extended': "Redis was only just being explored. I wrote my own functionaly for users to 'subscribe' to postbox's to recive updates.<br> When the server closed it persisted the world. <br>Learners had to choose and explore there own data structures as they deleloped there applications",
+        'year':'2009',
+        'team': 'solo',
+        'language': ['java'],
+        'category': 'code',
+        'width': 1,
+        'height': 1
+    },
+    
+    {
+        'title': 'orco',
+        'description': '',
+        'year': 2007
+    },
+
+    {
+        'title':'Learn2Care Booking System',
+        'link': '',
+        'image': 'l2c_booking.png',
+        'description': "Learn2Care was a career training company. They needed database consultancy for developing their data stores and required a custom event/room booking system with attendees and prices.",
+        'description_exended': "",
+        'year':'2006',
+        'team': 'solo',
+        'language': ['access','vb'],
+        'category': 'code',
+        'width': 1,
+        'height': 2
+    },
+
+    {
+        'title': 'baby godzilla suite',
+        'description': '',
+        'year': 2007,
+    },
+    
+    {
+        'title': 'Youth Group Summer Camp Volenteer',
+        'decription': 'DofE supervisor',
+        'year': '2005-2010',
+        'category': 'code',
+        'width': 1,
+        'height': 2
+    },
+
+    
+    {
+        'title':'Tile Based Game Framework',
+        'link': 'https://github.com/calaldees/javalib/tree/master/lib/Tile',
+        'image': 'dune.png',
+        'description': "A generic tile based network game framework, the base to support my UFO, Dune2 and C&C remakes",
+        'year':'2003-2009',
+        'team': 'solo',
+        'language': ['java'],
+        'category': 'code',
+        'width': 2,
+        'height': 2
+    },
+    
+    
+    {
+        'title': 'Advanced computing workshop',
+        'year': '2006',
+        'category': 'code',
+        'width': 1,
+        'height': 2
+    },
+
+    {
+        'title': 'Retro Console Shelfs',
+        'category': 'code',
+        'width': 1,
+        'height': 1
+    },
+
+    
+    {
+        'title':'GameFrame',
+        'link': 'https://github.com/calaldees/javalib/tree/master/Game/ProgrammingWorkshop',
+        'image': '',
+        'description': "A game lib for Java that gave learners simple access to display, timing, network and sound.",
+        'year': '2007',
+        'team': 'solo',
+        'language': ['java'],
+        'category': 'code',
+        'width': 1,
+        'height': 1
+    },
+
+    {
+        'title': 'Chaucer Technology School',
+        'description': 'AQA A2 Computing',
+        'year': '2005-2006',
+        'width': 2,
+        'height': 2
+    },
+    
+    {
+        'title': 'Arcade Cabinet',
+        'category': 'code',
+        'width': 2,
+        'height': 1
+    },
+
+    {
+        'title': 'purple tentacle',
+        'category': 'code',
+        'width': 1,
+        'height': 2
+    },    
+
+    
+
+    {
+        'title':'Mini Webserver',
+        'link': '',
+        'image': '',
+        'description': "Experiment to create a minimal HTTP file server in 100 lines of code.",
+        'description_extended': "Used as an example to teach learners that tech is understandable and achivable.",
+        'year':'2006',
+        'team': 'solo',
+        'language': ['java'],
+        'category': 'code',
+        'width': 1,
+        'height': 1
+    },
+
+    {
+        'title':'Bibliographic Database',
+        'link': '',
+        'image': '',
+        'description': "Web database of how research papers referenced each other",
+        'description_extended': "Research papers were Regex'ed to extract types of reference. Stored in DB. Users could them browse how papers were realted and who was references who.",
+        'year':'2003',
+        'team': 'solo',
+        'language': ['pl'],
+        'category': 'code',
+        'width': 1,
+        'height': 1
+    },
+    
+    {
+        'title':'UFO Enemy Unknown remake',
+        'link': '',
+        'image': 'ufo2.png',
+        'description': "Isometric map editor, troop animator, interactive inventory.",
+        'year':'2003',
+        'team': 'solo',
+        'language': ['java'],
+        'category': 'code',
+        'width': 2,
+        'height': 2
+    },
+
+    {
+        'title':'JHawk',
+        'link': '',
+        'image': '',
+        'description': "Webpage Regex hawk to create RSS feeds for when appropriate content on pages updated.",
+        'description_extended': "Many sites didnt support RSS for little details. I was spending too much time manually checking sites for driver updates etc. So I wrote my own tool to generate a custom RSS feed of all my sites but cunningly looking for specific DOM elements that had updated.</p> I was going to create a frontend for multiple users to registers searchs but never completed the public web interface.",
+        'year':'2006',
+        'team': 'solo',
+        'language': ['java'],
+        'category': 'code',
+        'width': 1,
+        'height': 1
+    },
+
+    {
+        'title': 'KenANIMEted',
+        'year': '1999-2013',
+        'category': 'code',
+        'width': 3,
+        'height': 1
+    },
+    
+    {
+        'title':'Material Modeller',
+        'link': '',
+        'image': 'springs.png',
+        'description': "Physics system based on masses and springs. User could interact in realtime and deform/break structures.",
+        'description_extended': "Final year research project into how real time simulation could help solve real problems. By modling simple physical rules the emergent behaviour was awesome, e.g friction with the floor made the wheels of a car actually turn.",
+        'year':'2002',
+        'team': 'solo',
+        'language': ['java'],
+        'category': 'code',
+        'width': 2,
+        'height': 2
+    },
+
+    {
+        'title':'Anime Society Loan System',
+        'link': '',
+        'image': 'anime_tracker.png',
+        'description': "Tracking over 1000 items in multiple locations. Members could reserve items for pickup.",
+        'description_exended': "Over 100 regular users. The library was distributed as personal items. The sytem allowed members to search and request items. The system then sent reminder emails and request emails before pickup time. Items had loan history and ratings.",
+        'year':'2001',
+        'team': 'solo',
+        'language': ['pl'],
+        'category': 'code',
+        'width': 2,
+        'height': 2
+    },
+
+    {
+        'title':'Ninja training grouds',
+        'link': '',
+        'image': '',
+        'description': "",
+        'description_exended': "",
+        'year':'2000',
+        'width': 1,
+        'height': 1
+    },
+
+    
+    {
+        'title':'Java Multimedia Player',
+        'link': '',
+        'image': '',
+        'description': "Create a slideshow with music and video. A custom UI experiment.",
+        'year':'2002',
+        'team': 'group',
+        'language': ['java'],
+        'category': 'code',
+        'width': 1,
+        'height': 1
+    },
+
+    {
+        'title': '80s Cartoon Preservation Project',
+        'year': '1998-2001',
+        'category': 'code',
+        'width': 1,
+        'height': 2
+    },
+
+    
+    {
+        'title':'Nortel ProSTAR Tracker',
+        'link': '',
+        'image': 'prostar_tracker2.png',
+        'description': "A web based bug status/reporting tool for management.",
+        'description_extended': "Custom HTML Graph output",
+        'year':'1999',
+        'team': 'solo',
+        'language': ['pl'],
+        'category': 'code',
+        'width': 1,
+        'height': 1
+    },
+    {
+        'title':'Cars',
+        'link': '',
+        'image': 'cars.png',
+        'description': "Micro machines style car game.",
+        'description_extended': "Had level editor, variable width font system and car physics. Wrote my own sprite rotation algorithum.",
+        'year':'1998',
+        'team': 'solo',
+        'language': ['pas'],
+        'category': 'code',
+        'width': 2,
+        'height': 2
+    },
+    {
+        'title':'Copter',
+        'link': '',
+        'image': 'chop.png',
+        'description': "Choplifter style helicopter game with random terrain generation.",
+        'description_exended': "Copter had physics, sprite animation. Terrain generation was a mathmatical experiment.",
+        'year':'1997',
+        'team': 'solo',
+        'language': ['pas'],
+        'category': 'code',
+        'width': 1,
+        'height': 1
+    },
+    {
+        'title':'Speed Test',
+        'link': '',
+        'image': 'speedtest.png',
+        'description': "Simple benchmark utility to test processor, ASCI, VGA and disk access.",
+        'description_exended': "The final scores were compared against known systems. The final output was displayed in a set of bar charts thats animated and grew.",
+        'year':'1996',
+        'team': 'solo',
+        'language': ['pas'],
+        'category': 'code',
+        'width': 1,
+        'height': 1
+    },
+
+    {
+        'title':'Retail Stock Control System',
+        'link': '',
+        'image': 'stock.png',
+        'description': "Real barcode checkout system written in Pascal. Created my own relational database structure with search/sort algorithms.",
+        'description_exended': "It could print new orders forms and stock charts. The grocery store it was designed for went bust before the project was finished.",
+        'year':'1998',
+        'team': 'solo',
+        'language': ['pas'],
+        'category': 'code',
+        'width': 1,
+        'height': 1
+    },
+    
+    {
+        'title': 'Code based animations',
+        'image': 'dnalogo.png',
+        'width': 1,
+        'height': 1
+    },
+    
+    {
+        'title':'Tracker Tunes',
+        'link': '',
+        'image': '',
+        'description': "",
+        'description_exended': "",
+        'year':'1996',
+        'team': 'solo',
+        'width': 1,
+        'height': 1
+    },
+    
+
+]
+);
