83f5b3f753679f2ad753c061f4a0806e41645f57
Allan Callaghan <calaldees@gmail.com>
2014-07-23 09:08:53 +0100

    Update to dependency download script

--- external/get_externals.sh
+++ external/get_externals.sh
@@ -1,4 +1,4 @@
-curl http://thing/jquery.min.js -O
-curl http://thing/masonry.min.js -O
-curl http://thing/css_reset.css
+curl http://code.jquery.com/jquery-2.1.1.min.js -o jquery.min.js
+curl http://masonry.desandro.com/masonry.pkgd.min.js -O
+curl http://yui.yahooapis.com/3.17.2/build/cssreset/cssreset-min.css -O
 
